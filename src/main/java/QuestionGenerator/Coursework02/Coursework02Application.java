11281f5c5e2ba1b8c266f8285fd74337d8977498
Сергей <lockup86@rambler.ru>
2024-09-10 20:57:49 +0300

    CW02

---
+++ src/main/java/QuestionGenerator/Coursework02/Coursework02Application.java
@@ -0,0 +1,13 @@
+package QuestionGenerator.Coursework02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework02Application.class, args);
+	}
+
+}
---
+++ src/test/java/QuestionGenerator/Coursework02/Coursework02ApplicationTests.java
@@ -0,0 +1,13 @@
+package QuestionGenerator.Coursework02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
